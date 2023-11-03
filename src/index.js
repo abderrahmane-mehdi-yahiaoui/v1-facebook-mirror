85694d4c3af988e7d1146bc2321289847df5468d
abderrahmane-mehdi-yahiaoui <mehdi.yahiaoui22@gmail.com>
2023-11-03 14:46:19 +0100

    removed report vital

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {App} from './App';
-import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './components/Context/AuthContext'
-import CatchError from './CatchError'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { App } from "./App";
+import { AuthProvider } from "./components/Context/AuthContext";
+import CatchError from "./CatchError";
 ReactDOM.render(
   <React.StrictMode>
     <CatchError>
@@ -12,13 +11,8 @@ ReactDOM.render(
         <Router>
           <App />
         </Router>
-    </AuthProvider>
+      </AuthProvider>
     </CatchError>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
