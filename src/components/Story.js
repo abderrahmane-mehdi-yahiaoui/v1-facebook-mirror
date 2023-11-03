7550d9621e302b5036c80a8519cf7f85e8d34da9
abderrahmane-mehdi-yahiaoui <mehdi.yahiaoui22@gmail.com>
2023-11-03 14:43:30 +0100

    updated dependencies

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar } from '@material-ui/core'
+import Avatar from "@mui/material/Avatar";
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { IconButton } from './Buttons'
